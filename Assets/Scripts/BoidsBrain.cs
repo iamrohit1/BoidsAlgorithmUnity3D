b0b39590afef9744f310a3f1a1e3c2975c2ca1e7
Rohit Nutalapati <iamrohit.n@gmail.com>
2021-04-04 17:24:17 -0500

    Initial Commit

---
+++ Assets/Scripts/BoidsBrain.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidsBrain : MonoBehaviour
+{
+    public int numBoids;
+    public float simulationSpeed;
+
+    public float coherence_bias;
+    public float collision_bias;
+    public float velocity_bias;
+    public float speed_bias;
+
+    public Coherence coherence;
+    public Collision collision;
+    public VelocityMatcher velocityMatcher;
+    
+    public GameObject boidPrefab;
+  
+    internal Boid[] boids;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Spawn random boids
+        boids = new Boid[numBoids];
+        for (int i = 0; i < numBoids; i++) {
+            GameObject b = Instantiate(boidPrefab,transform);
+            b.transform.position = new Vector3(Random.Range(CreateBounds.xMin, CreateBounds.xMax), Random.Range(CreateBounds.yMin, CreateBounds.yMax),0);
+            boids[i] = b.GetComponent<Boid>();
+            boids[i].direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            boids[i].speed = Random.Range(1,5);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //coherence
+        Vector2 coherenceVector;
+
+        //collision
+        Vector2 collisionVector;
+        Vector2 hardCollisionVector;
+        float hardCollisionBias;
+
+        //velocity match
+        Vector2 velocityMatcherVector;
+        float speedMatch;
+
+        // for each boid
+        for (int i = 0; i < boids.Length; i++)
+        {
+            //calculate modifications
+            coherenceVector = coherence.GetCoherence(boids[i]);
+            collisionVector = collision.GetSoftColissionAvoidance(boids[i]);
+            hardCollisionVector = collision.GetHardColissionAvoidance(boids[i], out hardCollisionBias);
+            velocityMatcherVector = velocityMatcher.GetVelocityMatchVector(boids[i], out speedMatch);
+
+            //add modifications to boid
+            boids[i].AdjustVelocityBy(coherenceVector, coherence_bias);
+            boids[i].AdjustVelocityBy(collisionVector, collision_bias);
+            boids[i].AdjustVelocityBy(velocityMatcherVector, velocity_bias);
+            boids[i].AdjustVelocityBy(hardCollisionVector, hardCollisionBias);
+            boids[i].AdjustSpeedBy(speedMatch, speed_bias);
+
+            //supply speed multiplier
+            boids[i].speedMultiplier = simulationSpeed;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/CreateBounds.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBounds : MonoBehaviour
+{
+    public float thickness;
+    public static float xMax;
+    public static float xMin;
+    public static float yMax;
+    public static float yMin;
+
+
+    GameObject[] bounds;
+    float orthographicSize;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetBounds();
+    }
+
+    void SetBounds() {
+
+        if (bounds != null) {
+            foreach (GameObject b in bounds)
+                Destroy(b);
+        }
+
+        if (Camera.main.orthographic)
+        {
+            bounds = new GameObject[4];
+
+            float hor = Camera.main.orthographicSize * Screen.width / Screen.height;
+            float ver = hor * Screen.height / Screen.width;
+
+            bounds[0] = SetBound(new Vector3(hor * 2, thickness, thickness), new Vector3(0, ver, 0));
+            bounds[1] = SetBound(new Vector3(hor * 2, thickness, thickness), new Vector3(0, -ver, 0));
+            bounds[2] = SetBound(new Vector3(thickness, ver * 2, thickness), new Vector3(hor, 0, 0));
+            bounds[3] = SetBound(new Vector3(thickness, ver * 2, thickness), new Vector3(-hor, 0, 0));
+
+            xMax = hor - thickness;
+            xMin = -xMax;
+            yMax = ver - thickness;
+            yMin = -yMax;
+
+            orthographicSize = Camera.main.orthographicSize;
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    GameObject SetBound(Vector3 localScale, Vector3 position)
+    {
+        GameObject bound = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        bound.layer = LayerMask.NameToLayer("HardCollision");
+        bound.transform.localScale = localScale;
+        bound.transform.position = Camera.main.transform.position + position + new Vector3(0,0, -Camera.main.transform.position.z);
+        bound.transform.parent = transform;
+
+        return bound;
+    }
+
+    private void Update()
+    {
+        if (orthographicSize != Camera.main.orthographicSize) {
+            SetBounds();
+        }
+    }
+}
